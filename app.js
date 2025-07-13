5ec3514cc4b30c88261adb0a9126695c588b0041
NO-Alim <abdulalimrakib53@gmail.com>
2025-07-13 22:47:35 +0600

    Front-end page added

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from "express";
-import notFound from "./middlewares/not-found.js";
+import path from "path";
+import { fileURLToPath } from "url";
 import errorHandler from "./middlewares/errorHandler.js";
+import notFound from "./middlewares/not-found.js";
 import taskRouter from "./routes/task.route.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/tasks", taskRouter);
 
